6c92df9282bc219c2d757b1932c11be5d51958e5
k-stiles <michealessuman08@gmail.com>
2024-10-28 21:10:18 +0000

    feat: parent --> child communication

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.scss',
+})
+export class ChildComponent {
+  @Input()
+  childVariable = '';
+
+  constructor() {
+    console.log('ChildComponent constructor');
+  }
+}
---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ChildComponent } from '../child/child.component';
+
+@Component({
+  selector: 'app-parent',
+  standalone: true,
+  imports: [ChildComponent],
+  templateUrl: './parent.component.html',
+  styleUrl: './parent.component.scss',
+})
+export class ParentComponent {
+  parentVariable = 'Stiles Stilinski is a character in Teen Wolf';
+
+  constructor() {
+    console.log('ParentComponent constructor');
+  }
+}
